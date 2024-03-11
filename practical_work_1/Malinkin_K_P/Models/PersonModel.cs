31d445c530161be5b5e5a7682b7407f0a0b1aa97
Kirill <m_kirill72@mail.ru>
2024-03-11 21:48:02 +0300

    Практическая_работа_1

---
+++ practical_work_1/Malinkin_K_P/Models/PersonModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malinkin_K_P.Models
+{
+    public class PersonModel : INotifyPropertyChanged
+    {
+        private string _firstName;
+        private string _lastName;
+        private int _age;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                if (_firstName != value)
+                {
+                    _firstName = value;
+                    OnPropertyChanged(nameof(FirstName));
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                if (_lastName != value)
+                {
+                    _lastName = value;
+                    OnPropertyChanged(nameof(LastName));
+                }
+            }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (_age != value)
+                {
+                    _age = value;
+                    OnPropertyChanged(nameof(Age));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ practical_work_1/Malinkin_K_P/View/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using Malinkin_K_P.Models;
+
+namespace Malinkin_K_P.View
+{
+    public class MainViewModel
+    {
+        public PersonModel Person { get; set; }
+
+        public MainViewModel()
+        {
+            Person = new PersonModel();
+        }
+    }
+}
